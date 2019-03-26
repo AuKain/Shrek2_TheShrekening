447c04b03e2024981538dc9a307aaa84fdad7e62
AuKain <nico.parr@hotmail.com>
2019-03-25 23:56:10 -0400

    Amorcé la préparation à l'organisation MVC.

---
+++ Modele/modele.php
@@ -0,0 +1,26 @@
+<?php
+
+// Effectue la connexion à la BDD
+// Instancie et renvoie l'objet PDO associé
+function getBdd() {
+    $bdd = new PDO('mysql:host=localhost;dbname=shrek2;charset=utf8', 'root', 'mysql', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    return $bdd;
+}
+
+// Renvoie les informations sur un event
+function getEvent($idEvent) {
+    $bdd = getBdd();
+    $event = $bdd->prepare('SELECT Events.event_id as "id", Places.place_id as "place_id", Players.player_id as "player_id", Events.event_name as "event_name", Places.place_name as "place_name", Players.name as "player_name", Events.event_description as "description", Events.other_event_details as "other_details" from Events left join Places on Events.place_id=Places.place_id left join Players on Events.player_id=Players.player_id where Events.event_id = ?');
+    $event->execute(array($idEvent));
+    if ($event->rowCount() == 1)
+        return $event->fetch();  // Accès à la première ligne de résultat
+    else
+        throw new Exception("Aucun article ne correspond à l'identifiant '$idEvent'");
+}
+
+// Renvoie la liste de tous les events
+function getEvents() {
+    $bdd = getBdd();
+    $events = $bdd->query('SELECT Events.event_id as "id", Places.place_id as "place_id", Players.player_id as "player_id", Events.event_name as "event_name", Places.place_name as "place_name", Players.name as "player_name", Events.event_description as "description", Events.other_event_details as "other_details" from Events left join Places on Events.place_id=Places.place_id left join Players on Events.player_id=Players.player_id order by Events.event_id');
+    return $events;
+}
---
+++ Vue/gabarit.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Shrek 2 the Movie the Game the TP</title>
+    </head>
+    <style>
+        form
+        {
+            text-align:center;
+        }
+    </style>
+    <body>
+        <a href="index.php"><h1>Shrek 2 the Movie the Game the TP</h1></a>
+        <?= $contenu ?>
+        <br /><a href=".\apropos.html">~~À propos~~</a>
+    </body>
+</html>
